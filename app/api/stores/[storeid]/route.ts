4e4700cae1b8cd99732499a6b5906cb212db3f56
Sevn_ <71951344+Bethel-nz@users.noreply.github.com>
2024-01-27 00:09:57 +0000

    feat: added delete route

--- app/api/stores/[storeid]/route.ts
+++ app/api/stores/[storeid]/route.ts
@@ -27,3 +27,23 @@ export async function PATCH(req: Request,{params:{storeid}}:{params:{storeid:str
 		return new NextResponse('Internal Server Error',{status : 500})
 	}
 }
+
+export async function DELETE(req: Request,{params:{storeid}}:{params:{storeid:string}}) {
+	try {
+		const { userId } = auth()
+		
+		if (!userId) return new NextResponse('Unauthenticated', { status: 401 })
+		if (!storeid) return new NextResponse('Store id is required', { status: 400 })
+		
+		const store = await prisma.store.deleteMany({
+			where: {
+				id: storeid,
+				userId
+			}
+		})
+		return NextResponse.json(store)
+	} catch (error) {
+		logger(`[STORE_DELETE]:`, error)
+		return new NextResponse('Internal Server Error',{status : 500})
+	}
+}
