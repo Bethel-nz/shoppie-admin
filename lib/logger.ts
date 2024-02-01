92cc6aa2178202f02ee435b0e071ff063a9e0aca
Sevn_ <71951344+Bethel-nz@users.noreply.github.com>
2024-02-01 18:39:58 +0000

    feat: renamed errorName to label

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,9 +1,9 @@
-export default function logger(errorName:string, error:any) {
+export default function logger(label:string, error:any) {
   const isDevelopment = process.env.NODE_ENV === 'development';
 
   if (isDevelopment) {
-    console.log(`[${errorName}]`, error);
+    console.log(`${label}`, error);
   } else {
-    throw new Error(`[${errorName}] ${error}`);
+    throw new Error(`${label} -::- ${error}`);
   }
 }
